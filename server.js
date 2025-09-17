b92b11f57cdde6e5f7fbbdeaeed8fc5dcca7550a
Soumya Chatterjee <soumyachatterjee@Soumyas-MacBook-Air.local>
2025-09-17 22:01:48 +0530

    api link add

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const app = express();
 const port = 3000; 
-const API_URL = "http://localhost:4000";
+const API_URL = "https://digital-bank-1-d91i.onrender.com";
 const FIXED_PASSWORD = "Soumya2802@";
 
 
